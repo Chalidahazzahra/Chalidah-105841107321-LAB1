0ae423a1ec06c9f96d065d9a4e9e7256d0ec80ea
Chalidahazzahra <137327985+Chalidahazzahra@users.noreply.github.com>
2024-06-14 07:34:42 +0000

    TextInput

--- App.js
+++ App.js
@@ -1,48 +1,37 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 
-const Buttoncostum = ({ color, text}) => {
-    return (
-        <View style={{ backgroundColor: color, width:160, height: 45, alignItems:'center', justifyContent: 'center',marginRight: 10, borderRadius: 15
-        }}>
-            <Text style={{ color:'white', fontSize: 20, fontWeight: 'bold'}}>
-                {text}
-            </Text>
-        </View>
-    );
-};
- const App = () =>{
-    return (
-        <View style={styles.container}>
-            <Text style={styles.topText}>
-                Take me out Indonesia
-            </Text>
-            <View style={{ flex:1, justifyContent:'center',alignItems:'center', marginTop: 75}}>
-                <Image source={{ uri:'image 1.png'}} style={styles.image}/>
-                <Buttoncostum color='red' text="login" />
-                <Buttoncostum color='blue' text="Sign Up" />    
-            </View>
-        </View>
-    );
- };
-
- const styles =StyleSheet.create({
-    container:{
-        flex: 1,
+const TextInputCustom = ({name, color, Password}) => {
+return (
+    <View style= {{
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    topText:{
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginVertical: 10,
-    },
-    image:{
-        width: 300,
-        height: 300,
-        marginBottom: 150,
-    },
- });
-
- export default App;
-
+    }}>
+        <TextInput  
+            placeholder={`Masukkan ${name}`}
+            style={{
+                height: 40,
+                borderColor: 'gray',
+                borderWidth: 1,
+                borderRadius: 10,
+                marginBottom: 10,
+                paddingLeft: 10,
+                color: color,
+            }}
+       />
+    </View>
+    )
+}
+const App = () => {
+ 
+return (
+    <View style= {{ flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }}>
+        <TextInputCustom name="Username" color="black" />
+        <TextInputCustom name="Password" color="red" />
+    </View>
+    )
+}
+export default App
