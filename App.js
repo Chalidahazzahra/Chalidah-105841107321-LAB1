5203474215976555f5e33339ff77994f058ab9fb
Chalidahazzahra <137327985+Chalidahazzahra@users.noreply.github.com>
2024-07-08 19:39:30 +0000

    SignUpPages

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const TextInputCustom = ({name, color, Password}) => {
+const TextInputCustom = ({name, color, secureTextEntry}) => {
 return (
     <View style= {{
         justifyContent: 'center',
@@ -20,6 +20,7 @@ return (
                 color: color,
                 fontFamily: 'MetroMedium'
             }}
+            secureTextEntry={secureTextEntry}
        />
     </View>
     )
@@ -27,46 +28,41 @@ return (
 const ButtonCustom = ({ color, text, onPress}) => {
   return (
     <TouchableOpacity onPress={onPress}>
-     <View style={{ backgroundColor: color, width:160, 
-            height: 45, alignItems:'center', 
-            justifyContent: 'center',textAlign: 'center', 
-            borderRadius: 10, width: 300, marginTop: 15, marginBottom:75
-        }}>
-        <Text style={{ color: 'white', fontSize: 20 }}>{text}</Text>
-      </View>
+        <View style={{ backgroundColor: color, height: 45, alignItems: "center", justifyContent: "center", 
+          textAlign: 'center', borderRadius: 10, width: 300, marginBottom: 100
+          }}>
+            <Text style= {{ color: 'white', fontSize: 20 }}>{text}</Text>
+        </View>
     </TouchableOpacity>
   );
 };
 
-const LoginPages = ({ navigation}) => { 
-return (
-    < View style={styles.container}>
+const SignUpPages = ({ navigation }) => {
+    return (
+    <View style={styles.container}>
         <Text style={styles.topText}>
-            Login
+            Sign up
         </Text>
     <View style= {{ flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     }}>
-        <TextInput placeholder="Email" style={styles.input} />
-        <TextInput placeholder="Password" secureTextEntry style={styles.input} />
-        <ButtonCustom color='violet' text="Login" onPress={() => alert('Login succesfull')} /> 
-        <TouchableOpacity style={styles.button} onPress={() => alert('Login succesfull')}>
-            <Text style={styles.ForgetPasswordText}>
-                Forget Password</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('ForgetPasswordPages')}>
-            <Text style={styles.topText2}>
-            Don't have an account? Sign Up</Text>
+        <TextInputCustom name="Username" color="black" />
+        <TextInputCustom name="Email" color="black" />
+        <TextInputCustom name="Password" color="black" secureTextEntry />
+        <ButtonCustom color='violet' text="Sign Up" onPress={() => alert ('Akun anda akan berhasil dibuat')} />
+        <TouchableOpacity onPress={() => navigation.navigate('LoginPages')}>
+            <Text style={styles.buttonText}>
+            or sign up with social account</Text>
         </TouchableOpacity>
         <View style={{ flex:1, justifyContent:'center',alignItems:'center', marginTop: 75, flexDirection: 'row'}}>
                 <Image source={{ uri:'google.png'}} style={styles.image}/> 
                 <Image source={{ uri:'facebook.png'}} style={styles.image}/>
-                </View>
             </View>
-        </View>
-    );
-  };
+    </View>
+    </View>
+        );
+    };
 
 const styles = StyleSheet.create({
     container: {
@@ -77,12 +73,17 @@ const styles = StyleSheet.create({
     topText: {
         fontSize: 50,
         textAlign: 'center',
-        marginRight: 150,
-        marginBottom: 100,
-        fontWeight: "bold",
+        marginRight: 130,
+        marginBottom: 80,
+        fontWeight: 'bold',
         fontFamily: 'MetroBold'
     },
-    ForgetPasswordText: {
+    topText1: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontFamily: 'MetroMedium'
+    },
+    buttonText: {
         fontSize: 15,
         textAlign: 'center',
         color: "gray",
@@ -93,25 +94,10 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
         borderRadius: 50,
-        marginHorizontal: 5
-    },
-    topText2: {
-        color: 'black',
-        marginTop: 20,
-        fontFamily: 'MetroMedium',
-    },
-    input: {
-       height: 45,
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 10,
         marginBottom: 10,
-        paddingLeft: 10,
-        width: 300,
+        marginHorizontal: 10,
         fontFamily: 'MetroMedium'
-    },
-    button: {
-        marginTop: 20
-    },
-})
-export default LoginPages;
+    }
+});
+
+export default SignUpPages;
