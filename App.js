acf3274d61546d4273c66ab754094eeb3e29661f
Chalidahazzahra <137327985+Chalidahazzahra@users.noreply.github.com>
2024-07-08 19:38:19 +0000

    LoginPages

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, } from 'react-native'
 import React from 'react'
 
-const TextInputCustom = ({name, color, secureTextEntry}) => {
+const TextInputCustom = ({name, color, Password}) => {
 return (
     <View style= {{
         justifyContent: 'center',
@@ -20,7 +20,6 @@ return (
                 color: color,
                 fontFamily: 'MetroMedium'
             }}
-            secureTextEntry={secureTextEntry}
        />
     </View>
     )
@@ -28,36 +27,47 @@ return (
 const ButtonCustom = ({ color, text, onPress}) => {
   return (
     <TouchableOpacity onPress={onPress}>
-        <View style={{ backgroundColor: color, height: 45, alignItems: "center", 
-          justifyContent: "center", textAlign: 'center', borderRadius: 10, width: 300, 
-          marginBottom: 100 }}>
-            <Text style= {{ color: 'white', fontSize: 15, fontFamily:'MetroMedium' }}>{text}</Text>
-        </View>
+     <View style={{ backgroundColor: color, width:160, 
+            height: 45, alignItems:'center', 
+            justifyContent: 'center',textAlign: 'center', 
+            borderRadius: 10, width: 300, marginTop: 15, marginBottom:75
+        }}>
+        <Text style={{ color: 'white', fontSize: 20 }}>{text}</Text>
+      </View>
     </TouchableOpacity>
-);
+  );
 };
-const ForgetPasswordPages = ({ navigation }) => {
+
+const LoginPages = ({ navigation}) => { 
 return (
     < View style={styles.container}>
         <Text style={styles.topText}>
-          Forget Password
-        </Text>
-        <Text style={styles.topText1}>
-        Please, enter your email address. You will receive a link to create a new password via email.
+            Login
         </Text>
     <View style= {{ flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 270,
     }}>
         <TextInput placeholder="Email" style={styles.input} />
-        <ButtonCustom color='violet' text="Reset Password" onPress={() => alert(' Reset link sind')}/>
-        <TouchableOpacity style={styles.button} onPress={() => alert(' Reset link sind')}>
+        <TextInput placeholder="Password" secureTextEntry style={styles.input} />
+        <ButtonCustom color='violet' text="Login" onPress={() => alert('Login succesfull')} /> 
+        <TouchableOpacity style={styles.button} onPress={() => alert('Login succesfull')}>
+            <Text style={styles.ForgetPasswordText}>
+                Forget Password</Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('ForgetPasswordPages')}>
+            <Text style={styles.topText2}>
+            Don't have an account? Sign Up</Text>
+        </TouchableOpacity>
+        <View style={{ flex:1, justifyContent:'center',alignItems:'center', marginTop: 75, flexDirection: 'row'}}>
+                <Image source={{ uri:'google.png'}} style={styles.image}/> 
+                <Image source={{ uri:'facebook.png'}} style={styles.image}/>
+                </View>
             </View>
         </View>
     );
-}; 
+  };
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -65,29 +75,43 @@ const styles = StyleSheet.create({
         padding: 70,
     },
     topText: {
-        fontSize: 30,
+        fontSize: 50,
         textAlign: 'center',
-        marginBottom: 10,
-        fontWeight: 'bold',
-        marginRight: 50,
+        marginRight: 150,
+        marginBottom: 100,
+        fontWeight: "bold",
         fontFamily: 'MetroBold'
     },
-    topText1: {
+    ForgetPasswordText: {
         fontSize: 15,
         textAlign: 'center',
-        marginTop: 50,
+        color: "gray",
+        marginBottom: 65,
         fontFamily: 'MetroMedium'
     },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        marginHorizontal: 5
+    },
+    topText2: {
+        color: 'black',
+        marginTop: 20,
+        fontFamily: 'MetroMedium',
+    },
     input: {
-        height: 45,
+       height: 45,
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 10,
         marginBottom: 10,
         paddingLeft: 10,
         width: 300,
-        color: 'black',
         fontFamily: 'MetroMedium'
     },
+    button: {
+        marginTop: 20
+    },
 })
-export default ForgetPasswordPages
+export default LoginPages;
